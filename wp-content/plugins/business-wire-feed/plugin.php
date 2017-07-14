a3afdfa4455a4e1cf48f7625bcff742e13c3d7b7
Aakash <akash.bhatia1184@gmail.com>
2017-07-14 18:32:45 +1000

    synch with latest versions

--- wp-content/plugins/business-wire-feed/plugin.php
+++ wp-content/plugins/business-wire-feed/plugin.php
@@ -11,24 +11,6 @@
 
 
 function BWFeed_showFeed(){
-	/* ERROR REPORTING */
-	//@ini_set( 'log_errors', 'Off' );
-	//@ini_set( 'display_errors', 'On' );
-	//define( 'WP_DEBUG', true );
-	//define( 'WP_DEBUG_LOG', false );
-	//define( 'WP_DEBUG_DISPLAY', true );
-
-	/*$feedUrl = "http://feed.businesswire.com/rss/finfeed/?rss=G1QFDERJXkJdFV1TWQ==";
-	$content = file_get_contents($feedUrl);
-	$x = new SimpleXmlElement($content);
-	$i = 0;
-	$items = array();
-	$items = $x;
-	$html = NULL;
-	$cwd = dirname(__FILE__);
-
-	file_put_contents($cwd.'/items.json', json_encode($items));*/
-	
 	$cwd = dirname(__FILE__);
 	$feed = file_get_contents($cwd . '/items.json');
 	$items = json_decode($feed);
